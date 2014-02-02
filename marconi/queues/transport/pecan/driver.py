cef3ded5b0a2852dd51d8e9753ed82126a458071
Balaji Iyer <balaji.iyer@rackspace.com>
2014-02-02 01:43:46 -0500

    Working version of Health endpoint with Pecan
    After battling for a longtime with stevedore, pecan configs, routing,
    misplaced root controllers, I now have a full working version of pecan
    in the marconi framework.
    
    Took me 6 hours to get here on a Saturday. I can’t believe Im not able
    to focus at work to get this done. Im touching this code after 3 weeks.

---
+++ marconi/queues/transport/pecan/driver.py
@@ -0,0 +1,149 @@
+# Copyright (c) 2013 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import abc
+#import falcon
+import functools
+import pecan
+from wsgiref import simple_server
+
+from oslo.config import cfg
+import six
+
+from marconi.common import decorators
+from marconi.common.transport import version
+from marconi.common.transport.wsgi import helpers
+from marconi.openstack.common.gettextutils import _
+import marconi.openstack.common.log as logging
+from marconi.queues import transport
+from marconi.queues.transport import auth, validation
+from marconi.queues.transport.pecan import config as api_config
+
+_WSGI_OPTIONS = [
+    cfg.StrOpt('bind', default='127.0.0.1',
+               help='Address on which the self-hosting server will listen'),
+
+    cfg.IntOpt('port', default=7777,
+               help='Port on which the self-hosting server will listen'),
+
+    cfg.IntOpt('content_max_length', default=256 * 1024),
+    cfg.IntOpt('metadata_max_length', default=64 * 1024)
+]
+
+_WSGI_GROUP = 'drivers:transport:pecan'
+
+LOG = logging.getLogger(__name__)
+
+
+def get_pecan_config():
+    # Set up the pecan configuration
+    filename = api_config.__file__.replace('.pyc', '.py')
+    return pecan.configuration.conf_from_file(filename)
+
+
+def setup_app(pecan_config=None, extra_hooks=None):
+    if not pecan_config:
+        pecan_config = get_pecan_config()
+
+    pecan.configuration.set_config(dict(pecan_config), overwrite=True)
+
+    app = pecan.make_app(
+        pecan_config.app.root,
+        static_root=pecan_config.app.static_root,
+        template_path=pecan_config.app.template_path,
+        debug=True,
+        force_canonical=getattr(pecan_config.app, 'force_canonical', True),
+        #hooks= DriverBase.before_hooks, #balajiiyer: rewrite this
+        # module to use pecan, instead if falcon
+        #wrap_app=middleware.ParsableErrorMiddleware,
+        guess_content_type_from_ext=False
+    )
+
+    return app
+
+
+@six.add_metaclass(abc.ABCMeta)
+class DriverBase(transport.DriverBase):
+
+    def __init__(self, conf, storage, cache, control):
+        super(DriverBase, self).__init__(conf, storage, cache, control)
+
+        #balajiiyer: im going to leave the wsgi options as it is for now
+        self._conf.register_opts(_WSGI_OPTIONS, group=_WSGI_GROUP)
+        self._wsgi_conf = self._conf[_WSGI_GROUP]
+        self._validate = validation.Validator(self._conf)
+
+        #balajiiyer: get pecan config here
+        pc = get_pecan_config()
+        pc.app.debug = True
+
+        #balajiiyer: setup bare minimum pecan app, routes and middleware later
+        self.app = setup_app(pecan_config=pc)
+
+        #self.app = None
+        #self._init_routes()
+        self._init_middleware()
+        #balajiiyer: calling bridge explicitly here
+        self.bridge
+
+    @decorators.lazy_property(write=False)
+    def before_hooks(self):
+        """Exposed to facilitate unit testing."""
+        return [
+            helpers.require_accepts_json,
+            helpers.extract_project_id,
+
+            # NOTE(kgriffs): Depends on project_id being extracted, above
+            functools.partial(helpers.validate_queue_identification,
+                              self._validate.queue_identification)
+        ]
+
+    def _init_routes(self):
+        """Initialize hooks and URI routes to resources."""
+        #self.app = falcon.API(before=self.before_hooks)
+        version_path = version.path()
+        for route, resource in self.bridge:
+            self.app.add_route(version_path + route, resource)
+
+    def _init_middleware(self):
+        """Initialize WSGI middlewarez."""
+
+        # NOTE(flaper87): Install Auth
+        if self._conf.auth_strategy:
+            strategy = auth.strategy(self._conf.auth_strategy)
+            self.app = strategy.install(self.app, self._conf)
+
+    @abc.abstractproperty
+    def bridge(self):
+        """Constructs a list of route/responder pairs that can be used to
+        establish the functionality of this driver.
+
+        Note: the routes should be unversioned.
+
+        :rtype: [(str, falcon-compatible responser)]
+        """
+        raise NotImplementedError
+
+    def listen(self):
+        """Self-host using 'bind' and 'port' from the WSGI config group."""
+
+        msgtmpl = _(u'Serving on host %(bind)s:%(port)s')
+        LOG.info(msgtmpl,
+                 {'bind': self._wsgi_conf.bind, 'port': self._wsgi_conf.port})
+
+        httpd = simple_server.make_server(self._wsgi_conf.bind,
+                                          self._wsgi_conf.port,
+                                          self.app)
+        httpd.serve_forever()
